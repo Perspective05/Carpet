4c40f07e4e452f5148e795385ac4caa26b79909c
Perspective05 <pertive05@gmail.com>
2022-04-07 20:15:13 +0900

    곤혹

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login(process.env.TOKEN);// 토큰 수정
+client.login(process.env.TOKEN);
